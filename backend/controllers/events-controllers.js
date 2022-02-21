e838dad68fdf800ae539d4a4cc6c1196577d6a09
JungHyun Yoo <jhyoo728@gmail.com>
2022-02-21 23:40:56 +0900

    Comment: Intersection Events
    고민중...

--- backend/controllers/events-controllers.js
+++ backend/controllers/events-controllers.js
@@ -4,7 +4,6 @@ const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
 
 const Event = require("../models/event-model");
-const Fixedevent = require("../models/fixedevent-model");
 
 const getEvents = async (req, res, next) => {
   let events;
@@ -19,6 +18,59 @@ const getEvents = async (req, res, next) => {
     events: events.map((event) => event.toObject({ getters: true })),
   });
 };
+
+const getIntersectionEventsByDay = async (req, res, next) => {
+  const inputCalendar = req.params.calendar;
+  const inputDate = req.params.date;
+  const timezone = req.params.timezone;
+  // .../api/calendar/:calendarId/date/:day
+  // .../api/calendar/:calendarId/month/:yearmonth
+  // .../api/calendar/:calendarId/fixed-event/timezone/:timezone
+  // input 정보로 Timezone도 필요함
+  // 쿼리를 쓰는 것이 좋은가? 필터를 사용해야 하는 것 같기도 하고..?
+  // API URL이 너무 길면 이해하기 힘든 것 아닌가...
+
+  // event model query
+  // event.calendar = inputCalendar
+  // startDate or endDate 의 년월일 = inputDate
+  // mongoose date 객체 검색법을 찾아봐야겠다.
+  // 검색 결과 패턴은 3가지.
+  // 시작과 끝이 모두 당일 / 시작은 당일, 끝은 다음날
+  // MongoDB의 이해도가 낮은 것 같다. 아직 RDB의 SQL처럼 사고하는 것 같다.
+  // 캘린더의 멤버 수가 아니라 그 날짜에 등록한 멤버를 구해야 할듯...
+
+  // 1. 특정 캘린더, 특정 날짜의 이벤트를 모두 불러옴
+  // 2. 특정 날짜에 이벤트를 등록한 사람 수
+  // 3. 겹치는 사람 수 = 특정 날짜에 이벤트를 등록한 사람 수
+
+  // 한명일 경우? 교집합 없음
+  // 두명 이상부터 -> 교집합 계산 시작
+  // 입력자 수 = 교집합 카운트 *BEST*
+  // 과반수 이상인 교집합을 구해서 ... 차순위로 보여주는 게 좋지 않을까?
+
+  // 결과를 순회하면서 배열을 3개 만듦.
+  // startTime, endTime, user
+  // startTime 의 갯수와 endTime의 갯수는 반드시 같음. user는 중복이 있을 수 있음.
+  // user 배열에서 입력자 수를 획득.
+  // startTime과 endTime은 세트로 계산해야함. 단순하게 값을 각각 따로 나열하여 비교하는 걸로는 교집합을 구할 수 없음...(어찌보면 당연한)
+  // startTime 의 오름차 순으로 객체를 정렬한 뒤, 15분 단위로 판단이 들어가야 하나? 이게 맞을 지도..
+  // 프론트에서 값 입력도 15분 단위로 받자.
+  // 루프는 좀 돌겠지만 이게 단순할지도...
+
+  // 교집합을 찾는 방법
+  // 교집합 객체: id, startTime, endTime
+  // 1. 15분 단위로 판단을 하여 교집합 카운터를 증가, 감소 시킴
+  // JS 배열 쓰는 법을 알아야 할듯... 객체를 빼내는 건 가능한가? key 항목만 있으면 가능한가?
+  // splice 함수를 사용하여 간단하게 가능! 그럼 이게 나을려나...
+  //
+  // 2. 2차원 배열을 만든다. 사람수 x 최소 시작 시간~최대 종료 시간 (15분 단위)
+  // 최소 시작 시간은 정렬하면 나오지만... 최대 종료 시간은 어떻게 구하지(종료시간 기준 정렬?)
+  // 시작시간은 S, 시작과 종료 사이는 1, 종료는 E, 빈 시간은 0
+  // S가 들어오면 시작시간 갱신, 카운터 증가, 멤버 추가
+  // E가 들어오면 종료시간 갱신, 교집합 갱신(카운터, 멤버, 시작&종료), 멤버 제외, 카운터 감소.
+};
+
+const getIntersectionEventsByMonth = async (req, res, next) => {};
 
 const getEventsByDate = async (req, res, next) => {
   const inputDate = req.params.date;
