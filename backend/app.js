f8fae3b8d24bef7e3352385cf96e953d12b03e23
JungHyun Yoo <jhyoo728@gmail.com>
2022-03-06 23:21:09 +0900

    !HOTFIX: Backend, Add HTTP ok status in header when preflight

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,9 @@ function createServer() {
       "Origin, X-Request-With, Content-Type, Accept, Authorization"
     );
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
     next();
   });
 
