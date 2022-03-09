6bf2aeb9b0f9fd16e1aa8c05c7dba50895949503
JungHyun Yoo <jhyoo728@gmail.com>
2022-03-09 21:21:18 +0900

    Refactor: Backend, rename, change path, remove file

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const HttpError = require("../models/http-error");
+const HttpError = require("../util/http-error");
 
 module.exports = (req, res, next) => {
   try {
