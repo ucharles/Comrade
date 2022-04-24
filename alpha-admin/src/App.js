a4978648fb9a6f401d83d78a074707aee124b43a
JungHyun Yoo <jhyoo728@gmail.com>
2022-04-24 21:47:55 +0900

    Refactor: Frontend, delete hash router

--- alpha-admin/src/App.js
+++ alpha-admin/src/App.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import MyLayout from "./layout/MyLayout";
 import { Admin, CustomRoutes, Resource, Authenticated } from "react-admin";
 import AuthProvider from "./users/controllers/AuthProvider";
@@ -16,92 +16,94 @@ import UserSetting from "./users/pages/UserSetting";
 import jsonServerProvider from "ra-data-json-server";
 
 const App = () => (
-  <Admin
-    layout={MyLayout}
-    loginPage={SigniInSide}
-    title={process.env.REACT_APP_TITLE}
-    dashboard={Dashboard}
-    authProvider={AuthProvider}
-    dataProvider={jsonServerProvider("https://jsonplaceholder.typicode.com")}>
-    <CustomRoutes>
-      <Route
-        path="/calendar/:cid"
-        element={
-          <Authenticated>
-            <Dashboard />
-          </Authenticated>
-        }
-      />
-      <Route
-        path="/calendar/:cid/date/:date"
-        element={
-          <Authenticated>
-            <Dashboard />
-          </Authenticated>
-        }
-      />
-      <Route
-        path="/calendar/new"
-        element={
-          <Authenticated>
-            <CreateCalendar />
-          </Authenticated>
-        }
-      />
-      <Route
-        path="/calendar/:cid/event"
-        element={
-          <Authenticated>
-            <AddEvents />
-          </Authenticated>
-        }
-      />
-      <Route
-        path="/calendar/:cid/edit"
-        element={
-          <Authenticated>
-            <EditEvents />
-          </Authenticated>
-        }
-      />
-      <Route
-        path="/settings"
-        element={
-          <Authenticated>
-            <UserSetting />
-          </Authenticated>
-        }
-      />
-      <Route
-        path="/calendar/settings"
-        element={
-          <Authenticated>
-            <EditCalendars />
-          </Authenticated>
-        }
-      />
-      <Route
-        path="/join/:uuid"
-        element={
-          <Authenticated>
-            <JoinCalendar />
-          </Authenticated>
-        }
-      />
-    </CustomRoutes>
-    <CustomRoutes noLayout>
-      <Route path="/signup" element={<SignUp />} />
-      <Route
-        path="/calendar/:cid/event/:date"
-        element={
-          <Authenticated>
-            <ViewDayEvent />
-          </Authenticated>
-        }
-      />
-    </CustomRoutes>
-    <Resource />
-  </Admin>
+  <BrowserRouter>
+    <Admin
+      layout={MyLayout}
+      loginPage={SigniInSide}
+      title={process.env.REACT_APP_TITLE}
+      dashboard={Dashboard}
+      authProvider={AuthProvider}
+      dataProvider={jsonServerProvider("https://jsonplaceholder.typicode.com")}>
+      <CustomRoutes>
+        <Route
+          path="/calendar/:cid"
+          element={
+            <Authenticated>
+              <Dashboard />
+            </Authenticated>
+          }
+        />
+        <Route
+          path="/calendar/:cid/date/:date"
+          element={
+            <Authenticated>
+              <Dashboard />
+            </Authenticated>
+          }
+        />
+        <Route
+          path="/calendar/new"
+          element={
+            <Authenticated>
+              <CreateCalendar />
+            </Authenticated>
+          }
+        />
+        <Route
+          path="/calendar/:cid/event"
+          element={
+            <Authenticated>
+              <AddEvents />
+            </Authenticated>
+          }
+        />
+        <Route
+          path="/calendar/:cid/edit"
+          element={
+            <Authenticated>
+              <EditEvents />
+            </Authenticated>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <Authenticated>
+              <UserSetting />
+            </Authenticated>
+          }
+        />
+        <Route
+          path="/calendar/settings"
+          element={
+            <Authenticated>
+              <EditCalendars />
+            </Authenticated>
+          }
+        />
+        <Route
+          path="/join/:uuid"
+          element={
+            <Authenticated>
+              <JoinCalendar />
+            </Authenticated>
+          }
+        />
+      </CustomRoutes>
+      <CustomRoutes noLayout>
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/calendar/:cid/event/:date"
+          element={
+            <Authenticated>
+              <ViewDayEvent />
+            </Authenticated>
+          }
+        />
+      </CustomRoutes>
+      <Resource />
+    </Admin>
+  </BrowserRouter>
 );
 
 export default App;
